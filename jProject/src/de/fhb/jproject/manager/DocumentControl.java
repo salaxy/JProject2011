969969a7435c7eddd706b4892f6aa434b64973b1
Michael.Koppen@googlemail.com <Michael.Koppen@googlemail.com@3001dfc2-75fc-425c-9051-ea58f1cd7904>
2011-06-06 01:26:40 +0000

    code kompakter gestaltet

--- jProject/src/de/fhb/jproject/manager/DocumentControl.java
+++ jProject/src/de/fhb/jproject/manager/DocumentControl.java
@@ -8,8 +8,8 @@ import org.orm.PersistentSession;
 
 public class DocumentControl {
 
-	User aktUser;
-	ProjectRolesControl projectRolesController;
+	private User aktUser;
+	private ProjectRolesControl projectRolesController;
 	
 	public DocumentControl(User aktUser, ProjectRolesControl projectRolesController){
 		
